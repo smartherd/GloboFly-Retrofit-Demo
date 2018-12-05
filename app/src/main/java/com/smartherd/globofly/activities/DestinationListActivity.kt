3b9ad1a556b5ccb4b40e2a6fe7183acc22533741
Smartherd <smartherd@gmail.com>
2018-12-05 15:20:44 +0530

    #3.5 First HTTP Request and Response

--- app/src/main/java/com/smartherd/globofly/activities/DestinationListActivity.kt
+++ app/src/main/java/com/smartherd/globofly/activities/DestinationListActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.smartherd.globofly.R
 import com.smartherd.globofly.helpers.DestinationAdapter
-import com.smartherd.globofly.helpers.SampleData
+import com.smartherd.globofly.models.Destination
+import com.smartherd.globofly.services.DestinationService
+import com.smartherd.globofly.services.ServiceBuilder
 import kotlinx.android.synthetic.main.activity_destiny_list.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DestinationListActivity : AppCompatActivity() {
 
@@ -31,7 +36,32 @@ class DestinationListActivity : AppCompatActivity() {
 
 	private fun loadDestinations() {
 
-        // To be replaced by retrofit code
-		destiny_recycler_view.adapter = DestinationAdapter(SampleData.DESTINATIONS)
+		val destinationService = ServiceBuilder.buildService(DestinationService::class.java)
+
+        val requestCall = destinationService.getDestinationList()
+
+        requestCall.enqueue(object: Callback<List<Destination>> {
+
+            override fun onResponse(call: Call<List<Destination>>, response: Response<List<Destination>>) {
+                if (response.isSuccessful) {
+                    val destinationList = response.body()!!
+                    destiny_recycler_view.adapter = DestinationAdapter(destinationList)
+                }
+            }
+
+            override fun onFailure(call: Call<List<Destination>>, t: Throwable) {
+
+
+            }
+        })
     }
 }
+
+
+
+
+
+
+
+
+
