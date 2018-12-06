d5c858ecc89e7d2d5c5eb793f5ddf9d9580957fb
Smartherd <smartherd@gmail.com>
2018-12-06 13:04:33 +0530

    #3.7 Logging Http Request and Response

--- app/src/main/java/com/smartherd/globofly/services/ServiceBuilder.kt
+++ app/src/main/java/com/smartherd/globofly/services/ServiceBuilder.kt
@@ -1,6 +1,7 @@
 package com.smartherd.globofly.services
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,8 +10,11 @@ object ServiceBuilder {
     // Before release, change this URL to your live server URL such as "https://smartherd.com/"
     private const val URL = "http://10.0.2.2:9000/"
 
+    // Create Logger
+    private val logger = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
     // Create OkHttp Client
-    private val okHttp = OkHttpClient.Builder()
+    private val okHttp = OkHttpClient.Builder().addInterceptor(logger)
 
     // Create Retrofit Builder
     private val builder = Retrofit.Builder().baseUrl(URL)
