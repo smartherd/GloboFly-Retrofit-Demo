a32b5c5d37c8aee2583d90288326ab7a253b4616
Smartherd <smartherd@gmail.com>
2018-12-06 12:15:06 +0530

    #3.6 Handling HTTP Response and Errors

--- app/src/main/java/com/smartherd/globofly/activities/DestinationListActivity.kt
+++ app/src/main/java/com/smartherd/globofly/activities/DestinationListActivity.kt
@@ -3,6 +3,7 @@ package com.smartherd.globofly.activities
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.smartherd.globofly.R
 import com.smartherd.globofly.helpers.DestinationAdapter
 import com.smartherd.globofly.models.Destination
@@ -42,16 +43,27 @@ class DestinationListActivity : AppCompatActivity() {
 
         requestCall.enqueue(object: Callback<List<Destination>> {
 
+            // If you receive a HTTP Response, then this method is executed
+            // Your STATUS Code will decide if your Http Response is a Success or Error
             override fun onResponse(call: Call<List<Destination>>, response: Response<List<Destination>>) {
                 if (response.isSuccessful) {
+                    // Your status code is in the range of 200's
                     val destinationList = response.body()!!
                     destiny_recycler_view.adapter = DestinationAdapter(destinationList)
+                } else if(response.code() == 401) {
+                    Toast.makeText(this@DestinationListActivity,
+                        "Your session has expired. Please Login again.", Toast.LENGTH_LONG).show()
+                } else { // Application-level failure
+                    // Your status code is in the range of 300's, 400's and 500's
+                    Toast.makeText(this@DestinationListActivity, "Failed to retrieve items", Toast.LENGTH_LONG).show()
                 }
             }
 
+            // Invoked in case of Network Error or Establishing connection with Server
+            // or Error Creating Http Request or Error Processing Http Response
             override fun onFailure(call: Call<List<Destination>>, t: Throwable) {
 
-
+                Toast.makeText(this@DestinationListActivity, "Error Occurred" + t.toString(), Toast.LENGTH_LONG).show()
             }
         })
     }
