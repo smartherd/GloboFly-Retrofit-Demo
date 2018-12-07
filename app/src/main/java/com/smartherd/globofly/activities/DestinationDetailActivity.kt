ae3f6a95ab1e87932b461e6146062ea84b4bca84
Smartherd <smartherd@gmail.com>
2018-12-07 16:28:25 +0530

    #4.3 Path Parameters

--- app/src/main/java/com/smartherd/globofly/activities/DestinationDetailActivity.kt
+++ app/src/main/java/com/smartherd/globofly/activities/DestinationDetailActivity.kt
@@ -4,57 +4,77 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import android.widget.Toast
 import com.smartherd.globofly.R
 import com.smartherd.globofly.helpers.SampleData
 import com.smartherd.globofly.models.Destination
+import com.smartherd.globofly.services.DestinationService
+import com.smartherd.globofly.services.ServiceBuilder
 import kotlinx.android.synthetic.main.activity_destiny_detail.*
+import retrofit2.Call
+import retrofit2.Response
 
 
 class DestinationDetailActivity : AppCompatActivity() {
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_destiny_detail)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_destiny_detail)
 
-		setSupportActionBar(detail_toolbar)
-		// Show the Up button in the action bar.
-		supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setSupportActionBar(detail_toolbar)
+        // Show the Up button in the action bar.
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-		val bundle: Bundle? = intent.extras
+        val bundle: Bundle? = intent.extras
 
-		if (bundle?.containsKey(ARG_ITEM_ID)!!) {
+        if (bundle?.containsKey(ARG_ITEM_ID)!!) {
 
-			val id = intent.getIntExtra(ARG_ITEM_ID, 0)
+            val id = intent.getIntExtra(ARG_ITEM_ID, 0)
 
-			loadDetails(id)
+            loadDetails(id)
 
-			initUpdateButton(id)
+            initUpdateButton(id)
 
-			initDeleteButton(id)
-		}
-	}
+            initDeleteButton(id)
+        }
+    }
 
-	private fun loadDetails(id: Int) {
+    private fun loadDetails(id: Int) {
 
-		// To be replaced by retrofit code
-		val destination = SampleData.getDestinationById(id)
+        val destinationService = ServiceBuilder.buildService(DestinationService::class.java)
+        val requestCall = destinationService.getDestination(id)
 
-		destination?.let {
-			et_city.setText(destination.city)
-			et_description.setText(destination.description)
-			et_country.setText(destination.country)
+        requestCall.enqueue(object : retrofit2.Callback<Destination> {
 
-			collapsing_toolbar.title = destination.city
-		}
-	}
+            override fun onResponse(call: Call<Destination>, response: Response<Destination>) {
 
-	private fun initUpdateButton(id: Int) {
+                if (response.isSuccessful) {
+                    val destination = response.body()
+                    destination?.let {
+                        et_city.setText(destination.city)
+                        et_description.setText(destination.description)
+                        et_country.setText(destination.country)
 
-		btn_update.setOnClickListener {
+                        collapsing_toolbar.title = destination.city
+                    }
+                } else {
+                    Toast.makeText(this@DestinationDetailActivity, "Failed to retrieve details", Toast.LENGTH_SHORT).show()
+                }
+            }
 
-			val city = et_city.text.toString()
-			val description = et_description.text.toString()
-			val country = et_country.text.toString()
+            override fun onFailure(call: Call<Destination>, t: Throwable) {
+                Toast.makeText(this@DestinationDetailActivity, "Failed to retrieve details " + t.toString(), Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun initUpdateButton(id: Int) {
+
+        btn_update.setOnClickListener {
+
+            val city = et_city.text.toString()
+            val description = et_description.text.toString()
+            val country = et_country.text.toString()
 
             // To be replaced by retrofit code
             val destination = Destination()
@@ -65,30 +85,30 @@ class DestinationDetailActivity : AppCompatActivity() {
 
             SampleData.updateDestination(destination);
             finish() // Move back to DestinationListActivity
-		}
-	}
+        }
+    }
 
-	private fun initDeleteButton(id: Int) {
+    private fun initDeleteButton(id: Int) {
 
-		btn_delete.setOnClickListener {
+        btn_delete.setOnClickListener {
 
             // To be replaced by retrofit code
             SampleData.deleteDestination(id)
             finish() // Move back to DestinationListActivity
-		}
-	}
+        }
+    }
 
-	override fun onOptionsItemSelected(item: MenuItem): Boolean {
-		val id = item.itemId
-		if (id == android.R.id.home) {
-			navigateUpTo(Intent(this, DestinationListActivity::class.java))
-			return true
-		}
-		return super.onOptionsItemSelected(item)
-	}
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == android.R.id.home) {
+            navigateUpTo(Intent(this, DestinationListActivity::class.java))
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
-	companion object {
+    companion object {
 
-		const val ARG_ITEM_ID = "item_id"
-	}
+        const val ARG_ITEM_ID = "item_id"
+    }
 }
--- app/src/main/java/com/smartherd/globofly/services/DestinationService.kt
+++ app/src/main/java/com/smartherd/globofly/services/DestinationService.kt
@@ -3,9 +3,13 @@ package com.smartherd.globofly.services
 import com.smartherd.globofly.models.Destination
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface DestinationService {
 
     @GET("destination")
     fun getDestinationList(): Call<List<Destination>>
+
+    @GET("destination/{id}")
+    fun getDestination(@Path("id") id: Int): Call<Destination>
 }
